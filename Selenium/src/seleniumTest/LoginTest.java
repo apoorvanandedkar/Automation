961ce5f5fe8baf7adab2b24f9fa3483eb0f88521
Apurva N <nandedkar.apurva@gmail.com>
2019-08-24 19:05:01 +0530

    SeleniumBankingsite

---
+++ Selenium/src/seleniumTest/LoginTest.java
@@ -0,0 +1,19 @@
+package seleniumTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTest {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\MyWork\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https:\\www.google.com");
+		driver.close();
+	}*/
+
+}
