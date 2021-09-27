8d016dc9d84a5d57f6fba12a3c887685b6fd0d18
Apurva N <nandedkar.apurva@gmail.com>
2021-09-27 11:28:55 +0530

    New classes

---
+++ MavenTestProject/src/test/java/qaclick/MavenTestProject/Abc.java
@@ -0,0 +1,24 @@
+package qaclick.MavenTestProject;
+
+import org.testng.annotations.Test;
+
+public class Abc {
+	
+	@Test
+	public void printA() 
+	{
+		System.out.println("A");
+	}
+	
+	@Test
+	public void printB() 
+	{
+		System.out.println("B");
+	}
+	@Test
+	public void printC() 
+	{
+		System.out.println("C");
+	}
+
+}
---
+++ MavenTestProject/src/test/java/qaclick/MavenTestProject/Xyz.java
@@ -0,0 +1,24 @@
+package qaclick.MavenTestProject;
+
+import org.testng.annotations.Test;
+
+public class Xyz {
+	
+	@Test
+	public void printX() 
+	{
+		System.out.println("X");
+	}
+	
+	@Test
+	public void printY() 
+	{
+		System.out.println("Y");
+	}
+	@Test
+	public void printZ() 
+	{
+		System.out.println("Z");
+	}
+
+}
